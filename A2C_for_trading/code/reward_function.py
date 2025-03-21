b4e498807b93d2f8003022a3772f7425bedfc48e
Abolfazle dehbozorgi <alic25119@gmail.com>
2025-03-21 08:54:25 +0330

    Update reward_function.py

--- A2C_for_trading/code/reward_function.py
+++ A2C_for_trading/code/reward_function.py
@@ -1,7 +1,7 @@
 
 def backup_reward_function(history):
 
-    portdo_return = (history['portfolio_valuation', -1] / history['portfolio_valuation', 0])
+    portfolio_return = (history['portfolio_valuation', -1] / history['portfolio_valuation', 0])
     market_return = (history['data_close', -1] / history['data_close', 0])
 
-    return (portdo_return - market_return)/market_return
+    return (portfolio_return - market_return)/market_return
