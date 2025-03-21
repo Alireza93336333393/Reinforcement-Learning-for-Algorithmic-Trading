bf2726a34df21ff232471630f780e5c186f66466
Abolfazle dehbozorgi <alic25119@gmail.com>
2025-03-21 08:51:38 +0330

    Update trainig_the_RL.py

--- A2C_for_trading/code/trainig_the_RL.py
+++ A2C_for_trading/code/trainig_the_RL.py
@@ -3,7 +3,7 @@ import pandas as pd
 from usefull import PreProcessing,for_alpha
 
 
-path = '/home/alireza/Desktop/alireza/jupyter_sttuf/bitcoin.csv'
+path = 'your_path.csv'
 
 
 df = pd.read_csv(path)
@@ -15,9 +15,9 @@ prep.add_return(df)
 df.columns = df.columns.str.lower()
 o,h,l,c,v,vwap,adv20,r = for_alpha(df)
 
-df['feature_alpha001'] = prep.alpha001(c,r)# this is ok the env went crazy becuse the agent takes bad action and end the monye
-df['feature_alpha041'] = prep.alpha041(h,l,vwap)#this is okay with the env !!
-df['feature_alpha054'] = prep.alpha054(o,h,l,c)#this is also okay with the env you can use it 
+df['feature_alpha001'] = prep.alpha001(c,r)
+df['feature_alpha041'] = prep.alpha041(h,l,vwap)
+df['feature_alpha054'] = prep.alpha054(o,h,l,c)
 prep.atr(df,20)
 prep.bb(df,length=20,std=2)
 df.fillna(0,inplace=True)
@@ -34,9 +34,9 @@ action_path = 'your_path.csv'
 
 env = gym.make("TradingEnv", df = df,positions = [-1,0,1],trading_fees=0.02,reward_function=backup_reward_function)
 
-model = A2C("MlpPolicy",env,device='cpu',tensorboard_log='./A2C_pnl+outbench')
+model = A2C("MlpPolicy",env,device='cpu',tensorboard_log='./your_path')
 
 
 model.learn(300_000,callback=ActionRecorderCallback(action_path=action_path)) 
 
-model.save(path='/home/alireza/Desktop/alireza/jupyter_sttuf/A2C_pnl+oubench_reward_function')
+model.save(path='your_path')
